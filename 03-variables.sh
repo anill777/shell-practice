b53e5334f0fda2f20c837b8b3fd3178ce8598db0
Anill <info@joindevops.com>
2024-08-28 17:14:05 +0530

    Names are taking from command prompt

---
+++ 03-variables.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+echo "Enter 2 Names of the persons"
+PERSON1=$1
+PERSON2=$2
+echo "$PERSON1: Hi $PERSON2, Good Morning"
+echo "$PERSON2: Good Morning $PERSON1"
+echo "$PERSON1: How are you $PERSON2"
+echo "$PERSON2: I am good, How are you $PERSON1"
+echo "$PERSON1: I am fine"
