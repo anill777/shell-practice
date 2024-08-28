28b210c91ca5be6139bb1301252c2d5fb5d95af3
Anill <info@joindevops.com>
2024-08-28 16:22:26 +0530

    Script is edited

--- HelloDevops.sh
+++ HelloDevops.sh
@@ -1,2 +1,7 @@
 #!/bin/bash
 echo "Hello world, I am Learning Devops and AWS"
+
+echo "Ramesh: Hi Suresh, Good Morning"
+echo "Suresh: Good Morning Ramesh"
+echo "Ramesh: How are you Suresh"
+echo "Suresh: I am good, How are you Ramesh"
