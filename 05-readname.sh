93aae57dc10ab30e3657829908297eed47133251
Anill <info@joindevops.com>
2024-08-28 18:56:23 +0530

    reading values from command prompt

--- 05-readname.sh
+++ 05-readname.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
-#echo "Please enter username"
-#read USERNAME 
-#echo "You enter username as : $USERNAME"
-echo "Please enter valid first number"
-read NUM1
-echo "Please enter valid second number"
-read NUM2
-SUM=$((NUM1+NUM2))
-echo "The Sum is: $SUM"
+echo "Please enter username"
+read USERNAME 
+echo "You enter username as : $USERNAME"
+#echo "Please enter valid first number"
+#read NUM1
+#echo "Please enter valid second number"
+#read NUM2
+#SUM=$((NUM1+NUM2))
+#echo "The Sum is: $SUM"
+echo "Please enter password"
+read PASSWD
+echo "You enter username as : $PASSWD"
