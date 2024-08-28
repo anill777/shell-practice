4e56009246b74a670e4148bca5eb09bdf819d85f
Anill <info@joindevops.com>
2024-08-28 16:47:29 +0530

    Script is changed with variables

--- 01-HelloDevops.sh
+++ 01-HelloDevops.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 echo "Hello world, I am Learning Devops and AWS"
-
-echo "Ramesh: Hi Suresh, Good Morning"
-echo "Suresh: Good Morning Ramesh"
-echo "Ramesh: How are you Suresh"
-echo "Suresh: I am good, How are you Ramesh"
-echo "Ramesh: I am fine"
+PERSON1=Ramesh
+PERSON2=Suresh
+echo "$PERSON1: Hi $PERSON2, Good Morning"
+echo "$PERSON2: Good Morning $PERSON1"
+echo "$PERSON1: How are you $PERSON2"
+echo "$PERSON2: I am good, How are you $PERSON1"
+echo "$PERSON1: I am fine"
