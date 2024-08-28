04b2a3db9d6f2a15c053a976a23fe2ae0ef5be8f
Anill <info@joindevops.com>
2024-08-28 19:38:40 +0530

    array of values

--- 06-array.sh
+++ 06-array.sh
@@ -3,3 +3,4 @@ PERSONS=("Sachin" "Dhoni" "Dravid")
 echo "First Person is: ${PERSONS[0]}"
 echo "second Person is: ${PERSONS[1]}"
 echo "Third Person is: ${PERSONS[2]}"
+echo "All Persons are: ${PERSONS[@]}"
