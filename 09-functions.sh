69eb8adf49d059b898982b298277be390d491a91
Anill <info@joindevops.com>
2024-08-30 12:25:24 +0530

    checking user is root or not

--- 09-functions.sh
+++ 09-functions.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
 user=$(id -u)
+date=$(date)
 if [ $user -ne 0 ]
 then
 echo "User not entered as root user"
+echo "date is: $date"
 exit 1
 else
 echo "User is root user"
+echo "date is: $date"
 fi
