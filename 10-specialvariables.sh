04ba52e00e6a735ec49182a3ad309c4eb620ceca
Anill <info@joindevops.com>
2024-08-30 12:55:29 +0530

    checking user is root or not

---
+++ 10-specialvariables.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+user=$(id -u)
+date=$(date +%F)
+R="\e[31m"
+G="\e[32m"
+N="\e[0m"
+Y="\e[33m"
+
+if [ $user -ne 0 ]
+then
+echo -e "$R User not entered as root user $N"
+echo "date is: $date"
+exit 1
+else
+echo -e "$G User is root user $N"
+#echo "date is: $date"
+fi
